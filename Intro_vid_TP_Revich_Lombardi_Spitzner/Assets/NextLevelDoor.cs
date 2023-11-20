729bebef27fbb7aafbd7b8bcbab12b213d1cdc48
aspitzner <aspitzner01@gmail.com>
2023-11-20 19:11:33 -0300

    Changes to intro scene

---
+++ Intro_vid_TP_Revich_Lombardi_Spitzner/Assets/NextLevelDoor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelDoor : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the collided object has the tag "NextLevel"
+        if (other.CompareTag("Player"))
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextSceneIndex = currentSceneIndex + 1;
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+
+
+}
